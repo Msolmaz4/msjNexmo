9c2d6a882ff6eafba4c54017cdca8a9cb5e319e5
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-19 16:46:11 +0200

    letyte

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser =require('body-parser')
-const Nexmo =require('nexmo')
+const Nexmo =require('@vonage/server-sdk')
 const ejs = require('ejs')
 const Port = 3000
 
@@ -13,11 +13,34 @@ app.engine('html',ejs.renderFile)
 app.get('/',(req,res)=>{
     res.render('index')
 })
+//bodyparser json formatinda gelen disyayi duyenlemeye yariyor
+app.use(bodyParser.json())
+//sifrelenmis body parserlda bu komut kullaniriy
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+
 
 //statik dosyalarin diyini belirtme
 
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname + '/statik'))
 
+//nexmo objwesi yapma
+
+const nexmo = new Nexmo({
+    apiKey: "badfa19e",
+    apiSecret: "USyGBUmV0yc4Gbs5"
+  },{debug:true})
+
+
+
+  //main.js den gelene yaklaamal
+app.post('/',(req,res)=>{
+    const number =req.body.number
+    const text =req.body.text
+
+    nexmo.message.sendSms("Vonage APIs",number,text)
+})
 
 //dinleme
 const server = app.listen(Port,()=>{
